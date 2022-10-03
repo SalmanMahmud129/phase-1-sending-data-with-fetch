db0f3deff0018aa38c324a630be81a3080b3b189
Salman Mahmud <salman_mahmud@hotmail.com>
2022-10-02 23:06:38 -0400

    commit tree 83b90f7d436ff466c27bacb4f724f11b9b4dcabf

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(name = 'test', email = 'test@test'){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+    
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })  
+        })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data.id)
+        // const idSpan = document.createElement('span')
+        const body = document.querySelector('body')
+        // idSpan.innerText(data.id)
+        // idSpan.append(body)
+        body.textContent = `${data.id}, ${data.name}, ${data.email} `
+        
+    })
+    .catch(err => {
+        const body = document.querySelector('body')
+        body.textContent = err.message
+    })
+
+}
+
+// const configurationObject = {
+//     method: "POST",
+    
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     }
+// }
